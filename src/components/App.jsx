af0b65b38a17359f1e0c3e285224d7a78dbd716a
Olena <newsroommy@gmail.com>
2023-02-03 05:21:06 +0300

    update App.jsx, index.js

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,69 +1,63 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import Section from './Section/Section';
 import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
 import Statistics from './Statistics/Statistics';
 import Notification from './Notification/Notification';
-export class App extends Component {
-  state = {
+
+const App = () => {
+  const [feedback, setFeedback] = useState({
     good: 0,
     neutral: 0,
     bad: 0,
-  };
+  });
   // перетворення об'єкта в масив
-  statePropNames = Object.keys(this.state);
-  //   функція, яка змінює state, feedback - це good, neutral, bad
-  onLeaveFeedback = feedback => {
-    // якщо нове значення залежить від попереднього - передається call-back
-    this.setState(prevState => {
-      // вираховувані властивості об'єкта
-      return { [feedback]: prevState[feedback] + 1 };
+  const statePropNames = Object.keys(feedback);
+
+  const onLeaveFeedback = feedback => {
+    setFeedback(prevState => {
+      // скільки попередніх відгуків
+      const value = prevState[feedback];
+      //розпилюємо попередні відгуки, і потім повертаємо нові відгуки
+      return { ...prevState, [feedback]: value + 1 };
     });
   };
-  countTotalFeedback() {
-    // деструктуризація об'єкту state
-    const { good, neutral, bad } = this.state;
-    const total = good + neutral + bad;
-    return total;
-  }
-  countPositiveFeedbackPercentage(propName) {
-    const total = this.countTotalFeedback();
+  const total = feedback.good + feedback.neutral + feedback.bad;
+  // propName - це - good, neutral,bad
+  const countPositiveFeedbackPercentage = propName => {
     if (!total) {
       return 0;
     }
-    // this.state.good
-    const value = this.state[propName];
+    const value = feedback[propName];
     // toFixed округлення 2 значень після коми
     const result = ((value / total) * 100).toFixed(2);
     // toFixed повертає строку - тому Number
     return Number(result);
-  }
+  };
 
-  render() {
-    const total = this.countTotalFeedback();
-    const positiveFeedback = this.countPositiveFeedbackPercentage('good');
-    const { good, neutral, bad } = this.state;
-    return (
-      <>
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            options={this.statePropNames}
-            onLeaveFeedback={this.onLeaveFeedback}
+  const positiveFeedback = countPositiveFeedbackPercentage('good');
+  const { good, neutral, bad } = feedback;
+  return (
+    <>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={statePropNames}
+          onLeaveFeedback={onLeaveFeedback}
+        />
+      </Section>
+      {total !== 0 ? (
+        <Section title="Statistics">
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            positiveFeedback={positiveFeedback}
           />
         </Section>
-        {this.countTotalFeedback() !== 0 ? (
-          <Section title="Statistics">
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={total}
-              positiveFeedback={positiveFeedback}
-            />
-          </Section>
-        ) : (
-          <Notification message="There is no feedback" />
-        )}
-      </>
-    );
-  }
-}
+      ) : (
+        <Notification message="There is no feedback" />
+      )}
+    </>
+  );
+};
+export default App;
